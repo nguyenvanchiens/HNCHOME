a2da4941467402aca6a2e7ffc0452fc1ac0adb07
quanlyflatshop <quanlyflatshop@gmail.com>
2022-04-21 08:32:54 +0700

    mdel

--- HNCHOME/Migrations/20220421012649_customize_Model.Designer.cs
+++ HNCHOME/Migrations/20220421012649_customize_Model.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HNCHOME.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HNCHOME.Migrations
 {
     [DbContext(typeof(HNCDbContext))]
-    partial class HNCDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220421012649_customize_Model")]
+    partial class customize_Model
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
