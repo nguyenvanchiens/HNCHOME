4498c028b73891016975f70f8369ee4df3ea5970
quanlyflatshop <quanlyflatshop@gmail.com>
2022-04-29 17:32:02 +0700

    ckeditor

--- HNCHOME/Controllers/ServiceController.cs
+++ HNCHOME/Controllers/ServiceController.cs
@@ -4,9 +4,51 @@ namespace HNCHOME.Controllers
 {
     public class ServiceController : Controller
     {
+        HNCDbContext _context;
+        public ServiceController(HNCDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_context.ServiceInfos.ToList());
+        }
+        public IActionResult ServiceRegistration(Customer customer, List<Guid> serviceId)
+        {
+            try
+            {
+                var customerModel = _context.Customers.Where(m => m.Email == customer.Email).FirstOrDefault();
+                if (customerModel != null)
+                {
+                    customer.CustomerId = customerModel.CustomerId;
+                }
+                else
+                {
+                    if (customer == null)
+                    {
+                        throw new Exception();
+                    }
+
+                    customer.CustomerId = Guid.NewGuid();
+                    customer.ModifiedBy = DateTime.Now.ToString();
+                    customer.CreatedDate = DateTime.Now;
+                    _context.Customers.Add(customer);
+                    foreach (var id in serviceId)
+                    {
+                        var customerServiceRegistration = new CustomerServiceRegistration();
+                        customerServiceRegistration.CustomerId = customer.CustomerId;
+                        customerServiceRegistration.ServiceId = id;
+                        _context.customerServiceRegistrations.Add(customerServiceRegistration);
+                    }
+                    _context.SaveChanges();
+                    return Ok(new { Res = "Successfully" });
+                }
+                throw new Exception("Error");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { Res = "Error" });
+            }
         }
     }
 }
